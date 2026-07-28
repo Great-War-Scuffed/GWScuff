41d9bd259505d4e08911ccbf0f152c18120b225c
jgDarkWolf <119447696+jgDarkWolf@users.noreply.github.com>
2026-07-27 23:13:06 -0400

    defines

--- common/defines/tgwr_defines.lua
+++ common/defines/tgwr_defines.lua
@@ -34,6 +34,14 @@ NDefines.NMilitary.MAX_DIVISION_SUPPORT_HEIGHT = 5
 NDefines.NMilitary.MAX_DIVISION_SUPPORT_WIDTH = 1
 
 -- Buildings
+NDefines.NNavy.NAVAL_INVASION_PREPARE_DAYS = 10            -- base days needed to prepare a naval invasion
+NDefines.NNavy.NAVAL_INVASION_PLAN_CAP = 50                  -- base cap of naval invasions can be planned at the same time
+NDefines.NNavy.BASE_NAVAL_INVASION_DIVISION_CAP = 10
+NDefines.NMilitary.PROMOTE_LEADER_CP_COST = 0.1
+NDefines.NMilitary.BATALION_CHANGED_EXPERIENCE_DROP = 0 -- TFB Style Converts
+NDefines.NMilitary.DEPLOY_TRAINING_MAX_LEVEL = 2 -- WAS 1 aka TRAINED | Since the above was changed there is no point to not allowing divs to be trained to regular considering that its only 10% stats now.
+NDefines.NMilitary.UNIT_LEADER_ASSIGN_TRAIT_COST = 0.1
+NDefines.NMilitary.COHESION_IMMOBILE_PLANNING_SPEED_MULTIPLIER = 1.0
 
 -- Halfing infra bonuses to account 5 extra infrastructure levels
 NDefines.NBuildings.INFRASTRUCTURE_RESOURCE_BONUS = 0.1             -- multiplicative resource bonus for each level of (non damaged) infrastructure
